fda4efb00f722f2f10df6fc9dcd16508cccd5134
ahmedoashery <ahmedoashery@gmail.com>
2021-05-23 17:28:17 +0200

    Set Charset And Collation To 'utf8mb4-utf8mb4_unicode_ci'

--- Expert/Migrations/202105231523212_InitializeDatabase.cs
+++ Expert/Migrations/202105231523212_InitializeDatabase.cs
@@ -7,6 +7,7 @@ namespace Expert.Migrations
     {
         public override void Up()
         {
+            Sql("ALTER SCHEMA `Expert`  DEFAULT CHARACTER SET utf8mb4  DEFAULT COLLATE utf8mb4_unicode_ci ;");
         }
         
         public override void Down()
