d65a2f7c2b6d91e106157ad2af31bcdfb110d4eb
ahmedoashery <ahmedoashery@gmail.com>
2021-05-23 22:02:43 +0200

    Add Customer And SalesOrder Model

---
+++ Expert/Data/Models/Customer.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+
+namespace Expert.Data.Models
+{
+    public class Customer
+    {
+        public Customer()
+        {
+            Orders = new List<SalesOrder>();
+        }
+
+        public int Customer_id { get; set; }
+        public string Code { get; set; }
+        public string Name { get; set; }
+
+        public virtual IList<SalesOrder> Orders { get; set; }
+    }
+}
---
+++ Expert/Data/Models/SalesOrder.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Expert.Data.Models
+{
+    public class SalesOrder
+    {
+        public int Order_id { get; set; }
+        public DateTime Date { get; set; }
+        public string Notes { get; set; }
+
+        public int Customer_id { get; set; }
+        public virtual Customer Customer { get; set; }
+    }
+}
